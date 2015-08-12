4c499a311b2642ff313805aa894557b4c517d93d
Amstrong Huang <chan32167@gmail.com>
2015-08-11 22:27:18 -0500

    2 Panels might be working
    2 Panels might be working

--- app/src/main/java/com/amstronghuang/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/amstronghuang/spotifystreamer/MainActivity.java
@@ -2,154 +2,23 @@ package com.amstronghuang.spotifystreamer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.amstronghuang.spotifystreamer.adapter.ArtistAdapter;
-import com.amstronghuang.spotifystreamer.model.ArtistDataSimple;
-import com.facebook.drawee.backends.pipeline.Fresco;
-
-import java.util.ArrayList;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
-import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.ArtistsPager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchFragment.ArtistSearchFragmentCallback{
 
-    @InjectView(R.id.artistsLV)
-    protected ListView artistsLV;
-
-    @InjectView(R.id.searchET)
-    protected EditText searchET;
-
-    @InjectView(R.id.messageTV)
-    protected TextView messageTV;
-
-    protected ArtistAdapter artistAdapter;
-
-    protected ArrayList<ArtistDataSimple> artistList;
-
-    private String searchText;
-
-    SpotifyApi api = new SpotifyApi();
-
-    SpotifyService spotify = api.getService();
-
-    private Handler handler = new Handler();
-
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            searchArtist();
-        }
-    };
-
+    boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mTwoPane = getResources().getBoolean(R.bool.sw600);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-        Fresco.initialize(this);
-
-        if (savedInstanceState == null || !savedInstanceState.containsKey("artistList")) {
-            artistList = new ArrayList<>();
-        } else {
-            artistList = savedInstanceState.getParcelableArrayList("artistList");
-        }
-
-        artistAdapter = new ArtistAdapter(this, artistList);
-
-        artistsLV.setAdapter(artistAdapter);
-
-        artistsLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent topSongsIntent = new Intent(MainActivity.this, TopSongsActivity.class);
-                ArtistDataSimple artist = (ArtistDataSimple) parent.getAdapter().getItem(position);
-                topSongsIntent.putExtra("idArtist", artist.getId());
-                topSongsIntent.putExtra("artist", artist.getArtistName());
-                startActivity(topSongsIntent);
-            }
-        });
-
-        searchET.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                searchText = s.toString();
-                if (s.toString().isEmpty()) {
-                    artistList.clear();
-                    artistAdapter.notifyDataSetChanged();
-                    messageTV.setText("");
-                } else {
-                    handler.removeCallbacks(runnable);
-                    handler.postDelayed(runnable, 300);
-                }
-            }
-        });
-
-    }
-
-    private void searchArtist(){
-        spotify.searchArtists(searchText, new Callback<ArtistsPager>() {
-                    @Override
-                    public void success(ArtistsPager artistsPager, Response response) {
-                        artistList.clear();
-                        for (Artist artist : artistsPager.artists.items) {
-                            artistList.add(new ArtistDataSimple(artist.id, artist.images != null && !artist.images.isEmpty() ? artist.images.get(0).url : null, artist.name));
-                        }
-                        MainActivity.this.runOnUiThread(new Runnable() {
-                            public void run() {
-                                artistAdapter.notifyDataSetChanged();
-                                if (artistList.isEmpty()) {
-                                    messageTV.setText("No results, please refine search");
-                                } else {
-                                    messageTV.setText("");
-                                }
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-
-                    }
-                }
-
-        );
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putParcelableArrayList("artistList", artistList);
-        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -172,5 +41,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(String artistID, String artistName) {
+        if (mTwoPane)
+        {
+            //TopSongsFragment topTracksFragment =
+             //       TopSongsFragment.newInstance(artistID, artistName);
+
+            //getSupportFragmentManager().beginTransaction()
+             //       .replace(R.id.top_tracks_container, topTracksFragment).commit();
+        } else
+        {
+            Intent topSongsIntent = new Intent(this, TopSongsActivity.class);
+            //ArtistDataSimple artist = (ArtistDataSimple) parent.getAdapter().getItem(position);
+            topSongsIntent.putExtra("idArtist", artistID);
+            topSongsIntent.putExtra("artist", artistName);
+            startActivity(topSongsIntent);
+        }
     }
 }
---
+++ app/src/main/java/com/amstronghuang/spotifystreamer/SearchFragment.java
@@ -0,0 +1,156 @@
+package com.amstronghuang.spotifystreamer;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.amstronghuang.spotifystreamer.adapter.ArtistAdapter;
+import com.amstronghuang.spotifystreamer.model.ArtistDataSimple;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+public class SearchFragment extends Fragment {
+
+    @InjectView(R.id.artistsLV)
+    protected ListView artistsLV;
+
+    @InjectView(R.id.searchET)
+    protected EditText searchET;
+
+    @InjectView(R.id.messageTV)
+    protected TextView messageTV;
+
+    protected ArtistAdapter artistAdapter;
+
+    protected ArrayList<ArtistDataSimple> artistList;
+
+    private String searchText;
+
+    SpotifyApi api = new SpotifyApi();
+
+    SpotifyService spotify = api.getService();
+
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            searchArtist();
+        }
+    };
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_search_main, container, false);
+        ButterKnife.inject(this, rootView);
+        Fresco.initialize(this.getActivity());
+
+        if (savedInstanceState == null || !savedInstanceState.containsKey("artistList")) {
+            artistList = new ArrayList<>();
+        } else {
+            artistList = savedInstanceState.getParcelableArrayList("artistList");
+        }
+
+        artistAdapter = new ArtistAdapter(this.getActivity(), artistList);
+
+        artistsLV.setAdapter(artistAdapter);
+
+        artistsLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ArtistDataSimple artist = (ArtistDataSimple) parent.getAdapter().getItem(position);
+                ((MainActivity) getActivity()).onItemSelected(artist.getId(), artist.getArtistName());
+            }
+        });
+
+        searchET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                searchText = s.toString();
+                if (s.toString().isEmpty()) {
+                    artistList.clear();
+                    artistAdapter.notifyDataSetChanged();
+                    messageTV.setText("");
+                } else {
+                    handler.removeCallbacks(runnable);
+                    handler.postDelayed(runnable, 300);
+                }
+            }
+        });
+        return rootView;
+    }
+
+    private void searchArtist(){
+        spotify.searchArtists(searchText, new Callback<ArtistsPager>() {
+                    @Override
+                    public void success(ArtistsPager artistsPager, Response response) {
+                        artistList.clear();
+                        for (Artist artist : artistsPager.artists.items) {
+                            artistList.add(new ArtistDataSimple(artist.id, artist.images != null && !artist.images.isEmpty() ? artist.images.get(0).url : null, artist.name));
+                        }
+                        getActivity().runOnUiThread(new Runnable() {
+                            public void run() {
+                                artistAdapter.notifyDataSetChanged();
+                                if (artistList.isEmpty()) {
+                                    messageTV.setText("No results, please refine search");
+                                } else {
+                                    messageTV.setText("");
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+
+                    }
+                }
+
+        );
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("artistList", artistList);
+        super.onSaveInstanceState(outState);
+    }
+
+    public interface ArtistSearchFragmentCallback
+    {
+        void onItemSelected(String artistID, String artistName);
+    }
+
+}
